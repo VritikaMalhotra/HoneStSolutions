0569f83e4d021bb1fbdf51e7055de793c74ee779
marz-20 <170320116057.it.mayank@gmail.com>
2021-08-11 21:46:13 +0530

    test commit

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,10 +1,6 @@
 @extends('app')
 
-<<<<<<< HEAD:resources/views/servies.blade.php
-@section('curosal-title',('SERVCIES'))
-=======
 @section('curosal-title',('SERVICE'))
->>>>>>> 80c1bd0884eb67bc95daf54a8d8d4f128c1d57f7:resources/views/service.blade.php
 @section('curosal-subtitle',('together we can make a difference'))
     @section('main-content')
         @include('about/curosel')
