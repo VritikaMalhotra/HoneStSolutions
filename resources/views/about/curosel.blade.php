2236137f9a2ecd58f845d07cf14fabf07525c0c5
herat <herat13198@gmail.com>
2021-08-12 15:24:20 +0530

    footer change

--- resources/views/about/curosel.blade.php
+++ resources/views/about/curosel.blade.php
@@ -74,29 +74,21 @@
             </ul>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-<<<<<<< HEAD
-                <img src="img/anshuman-abhishek-d2Uk2kqNM3Y-unsplash.jpg" alt="Los Angeles" width="100%" height="100%">
-=======
-                <img src="img/temple.jpg" alt="Los Angeles" width="100%" height="60%">
->>>>>>> a9b24de8e5cc4d992d7c9ce28daaa9d02211ee7d
+                <img src="img/temple.jpg" alt="Los Angeles" width="100%" height="100%">
                 <div class="carousel-content">
                     <h1 class="carousel__title mb-0">@yield('curosal-title')</h1>
                     <p class="carousel__subtitle mb-0">@yield('curosal-subtitle')</p>
                 </div>
                 </div>
                 <div class="carousel-item">
-<<<<<<< HEAD
-                <img src="img/anshuman-abhishek-d2Uk2kqNM3Y-unsplash.jpg" alt="Chicago" width="100%" height="100%">
-=======
-                <img src="img/temple.jpg" alt="Chicago" width="100%" height="60%">
->>>>>>> a9b24de8e5cc4d992d7c9ce28daaa9d02211ee7d
+                <img src="img/temple.jpg" alt="Chicago" width="100%" height="100%">
                 <div class="carousel-content">
                     <h1 class="carousel__title mb-0">@yield('curosal-title')</h1>
                     <p class="carousel__subtitle mb-0">@yield('curosal-subtitle')</p>
                 </div>
                 </div>
                 <div class="carousel-item">
-                <img src="img/anshuman-abhishek-d2Uk2kqNM3Y-unsplash.jpg" alt="New York" width="100%" height="100%">
+                <img src="img/temple.jpg" alt="New York" width="100%" height="100%">
                 <div class="carousel-content">
                     <h1 class="carousel__title mb-0">@yield('curosal-title')</h1>
                     <p class="carousel__subtitle mb-0">@yield('curosal-subtitle')</p>
