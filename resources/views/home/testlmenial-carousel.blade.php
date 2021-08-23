a51cc2ef712353838906c4281004aa829afb0750
marz-20 <170320116057.it.mayank@gmail.com>
2021-08-23 22:52:48 +0530

    testimonial and banner images

--- resources/views/home/testlmenial-carousel.blade.php
+++ resources/views/home/testlmenial-carousel.blade.php
@@ -115,6 +115,13 @@
                      <li data-target="#testomoials-carousel" data-slide-to="0" class="active"></li>
                      <li data-target="#testomoials-carousel" data-slide-to="1"></li>
                      <li data-target="#testomoials-carousel" data-slide-to="2"></li>
+                     <li data-target="#testomoials-carousel" data-slide-to="3"></li>
+                     {{-- <li data-target="#testomoials-carousel" data-slide-to="4"></li>
+                     <li data-target="#testomoials-carousel" data-slide-to="5"></li>
+                     <li data-target="#testomoials-carousel" data-slide-to="6"></li>
+                     <li data-target="#testomoials-carousel" data-slide-to="7"></li>
+                     <li data-target="#testomoials-carousel" data-slide-to="8"></li>
+                     <li data-target="#testomoials-carousel" data-slide-to="9"></li> --}}
                  </ul>
                  <div class="carousel-inner">
                      <div class="carousel-item active">
@@ -147,6 +154,22 @@
                          </div>
                         </div>
                     </div>
+
+                    <div class="carousel-item">
+                        <div class="d-flex justify-content-center">
+                            <img src="img/People1.jpg" alt="" class="testomoials-carousel__image-fix">
+                        </div>
+                        <div class="testomoials-carousel__content">
+                            <h1 class="testomoials-carousel__name pt20">Mr. HellpGulati, CEO, My Car My Ride</h1>
+                            <p class="testomoials-carousel__description">I distinctly remember going to a workshop conducted by Mr Prasun Kundu at AMA, Ahmedabad on the topic, “Discover the Arjuna in You". The workshop was nice but I was awestruck by Prasun's pronunciation of the shlokas of Bhagavad Gita. That compelled me to request Mr Prasun to help me learn, "How to read and chant Bhagavad Gita Shlokas". We started with Chapter 18 and I got stuck on the first shloka for 20 mins as I had no background of Sanskrit nor had gone through Bhagavad Gita ever. Such speed breakers came in pretty regularly for me. It was Prasun's patience and methodology of teaching that helped me learn what I could. Now for anyone reading this,  I would like to share that I go through Bhagavad Gita shlokas every day. It helps me ease down, betters my composure, solves a lot of day to day problems, and most importantly, I have now taken the first step towards the spiritual journey. Whenever I am sad or in any difficult situation, learnings from Bhagavad Gita helps me overcome delusion each and every time. Thanks to Mr Prasun Kundu for making my life easier.
+                           </p>
+                        </div>
+                       </div>
+                   </div>
+                   
+               
+           </div>
+
                 </div>
                 <div class="d-flex justify-content-center pt30">
                     <a href="/people" class="btn btn-orange">Read more</a>
