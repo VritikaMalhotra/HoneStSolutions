f19e913c4d34e87433ccba8564fba8694d62dc4d
VritikaMalhotra <vritikamalhotra23@gmail.com>
2021-08-25 23:17:18 +0530

    Done most of chnages in various pages

--- resources/views/travel/blog1.blade.php
+++ resources/views/travel/blog1.blade.php
@@ -1,46 +1,54 @@
 <html>
-    <head>
+
+<head>
     <style>
-        .travel-details__image-fix{
-            width:100%;
+        .travel-details__image-fix {
+            width: 100%;
         }
-
-
-
     </style>
-    </head>
-    <body>
-        <div class="page-travel-details py-5">
-            <div class="container">
-                <h1 class="travel-details__title">Help Children To Get Food</h1>
-                <p class="travel-details__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.</p>
-                <p class="travel-details__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.</p>
-                <div class="travel-details__warpper justify-content-center row m-0">
-                    <a href="#" class="travel-details__image col-sm-6 pt-3">
-                        <img src="img/person_1.jpg" alt="" class="travel-details__image-fix">
-                    </a>
+</head>
 
-                    <a href="#" class="travel-details__image col-sm-6 pt-3">
-                        <img src="img/person_2.jpg" alt="" class="travel-details__image-fix">
-                    </a>
+<body>
+    <div class="page-travel-details py-5">
+        <div class="container">
+            <h1 class="travel-details__title">Geeta Chanting is Enchanting</h1>
+            <p class="travel-details__description">तद्विद्धि प्रणिपातेन परिप्रश्नेन सेवया |<br>
+                उपदेक्ष्यन्ति ते ज्ञानं ज्ञानिनस्तत्त्वदर्शिन: || 34||<br><br>
 
-                </div>
-                <p class="travel-details__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.</p>
-                <p class="travel-details__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.</p>
-                <div class="travel-details__warpper justify-content-center row m-0">
-                    <a href="#" class="travel-details__image col-sm-6 pt-3">
-                        <img src="img/person_1.jpg" alt="" class="travel-details__image-fix">
-                    </a>
 
-                    <a href="#" class="travel-details__image col-sm-6 pt-3">
-                        <img src="img/person_2.jpg" alt="" class="travel-details__image-fix">
-                    </a>
+                "Learn the Truth by approaching a spiritual master. Inquire from him with reverence and render service unto him. Such an enlightened Saint can impart knowledge unto you because he has seen the Truth. " Verse 34, Chapter 4, Shrimad Bhagavad Gita
+            </p>
+            <p class="travel-details__description">The Vedic scriptures advise us repeatedly on the importance of the Guru on the spiritual path. "Only through a Guru can you understand the Vedas.”
+                What does the word Guru mean - gu stands for darkness, while ru stands for light. The term Guru is therefore explained as the remover of darkness, one who reveals the light of the heart. </p>
+            <div class="travel-details__warpper justify-content-center row m-0">
+                <!-- <a href="#" class="travel-details__image col-sm-6 pt-3">
+                    <img src="img/person_1.jpg" alt="" class="travel-details__image-fix">
+                </a>
 
-                </div>
-                <p class="travel-details__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.</p>
-                <p class="travel-details__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis hic dolorum facere, autem cupiditate harum placeat quibusdam molestias! Nemo eos explicabo id sit possimus ad! Blanditiis dolore eius voluptate perferendis.</p>
-                <a href="#" class="btn btn-orange">Comment Now</a>
+                <a href="#" class="travel-details__image col-sm-6 pt-3">
+                    <img src="img/person_2.jpg" alt="" class="travel-details__image-fix">
+                </a> -->
+
             </div>
+            <p class="travel-details__description">Three years ago, when we decided to study the Bhagavad Gita, we realized that it is a formidable task and cannot be done on our own. It has to be learnt and read in the correct way. A search for Gita Chanting classes led us to the Chinmaya Mission. The course had ended a few days ago, but they gave us the teachers number. The teacher agreed to teach us at her place, and asked us to come the next day. And it started... For more than a month we learnt the chanting of Chapter 18 of the Bhagavad Gita. She did not charge a single penny, and instead asked us to donate to the Mission. With the promise of contacting us when the next course started. </p>
+            <p class="travel-details__description">In December, when we joined the six month Geeta chanting course, we met her again. This time it was a more detailed learning. Never came across a more selfless teacher than her. An ever smiling face and always willing to answer questions. Her knowledge on the subject is immense. She always had endless stories and incidences to tell us. Turned up for every single class. Once she could hardly speak due to a sore throat, but still she came. A blessing to have such a teacher. </p>
+            <div class="travel-details__warpper justify-content-center row m-0">
+                <!-- <a href="#" class="travel-details__image col-sm-6 pt-3">
+                    <img src="img/person_1.jpg" alt="" class="travel-details__image-fix">
+                </a>
+
+                <a href="#" class="travel-details__image col-sm-6 pt-3">
+                    <img src="img/person_2.jpg" alt="" class="travel-details__image-fix">
+                </a> -->
+
+            </div>
+            <p class="travel-details__description">"Geeta chanting is enchanting." Truly so. Our Geeta Chanting course came to an end after six months and we were awarded certificates on the Guru Purnima day, by Swami Avyanandji, the head of the local Chinmaya Mission.
+We are making a humble effort to share our learning with people who want to explore the power of "Gita Chanting"...</p>
+            <p class="travel-details__description">Please feel free to contact us as - Gita Chanting is Enchanting<br>
+Sandhya Prasun Kundu</p>
+            
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
