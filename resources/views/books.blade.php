50f2461d89327af825958886b654d0630ca48f71
LayShah5300 <layshahbhuj@gmail.com>
2021-08-28 15:45:06 +0530

    FOr reset

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,15 +1,8 @@
 @extends('app')
 
-<?php $page= 'books'; ?>
+@section('curosal-title',('Bhagavad Gita'))
+@section('curosal-subtitle',(''))
 
-
-@section('curosal-background',('ved2.jpg'))
-@section('curosal-title',('Book'))
-@section('curosal-subtitle',('The Corporate Bhagavad Gita'))
-@section('succes_story__image',('Apple21-10-2018.png'))
-@section('succes_story__title',('Discover The Arjuna in You'))
-@section('succes_story__title-second',('Apple.inc'))
-@section('succes_story__subtitle',('Our two sessions of "Discover the Arjuna in You" were held with the employees of Apple, West India Zone comprising employees from Gujarat, Maharastra and MP '))
 @section('main-content')
     @include('about/curosel')
     @include('book/buy-book')
