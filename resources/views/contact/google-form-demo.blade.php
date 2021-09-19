8033db46bc471c122f4dbbc5eaae6a6d967ea37c
heratshah <herat13198@gmail.com>
2021-09-19 15:21:37 +0530

    form done succes

--- resources/views/contact/google-form-demo.blade.php
+++ resources/views/contact/google-form-demo.blade.php
@@ -12,68 +12,40 @@
 onload="if(submitted) {
 $('#exampleModal').modal('toggle')
 }"></iframe>
-    <form method="POST" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScsH4KEBzW89xpDoOB3BX6l2x-i5he1GeyST0Z8Dml4h2cv3w/formResponse"
+    <form method="POST" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeCYq31EALEXFUxLY7SLToQbVdNWIPSU_2thC0y4C_swDGkfA/formResponse"
         target="hidden_iframe" onsubmit="submitted=true;"
          >
        
         <div class="form-group">
             <label for="your_email">Email</label>
-            <input name="entry.1045781291" type="email" class="form-control" id="your_email" aria-describedby="emailHelp" placeholder="Enter email">
+            <input name="entry.920284097" type="email" class="form-control" id="your_email" aria-describedby="emailHelp" placeholder="Enter email" required>
         </div>
         <div class="form-group">
             <label for="your_name">Full Name</label>
-            <input name="entry.2005620554" type="text" class="form-control" id="your_name" placeholder="Enter name">
+            <input name="entry.1104963041" type="text" class="form-control" id="your_name" placeholder="Enter name" required>
         </div>
         <div class="form-group">
             <label for="your_age">Age</label>
-            <input name="entry.1065046570" type="text" class="form-control" id="your_age" placeholder="Enter age">
+            <input name="entry.1091435893" type="text" class="form-control" id="your_age" placeholder="Enter age" required>
         </div>
         
   
         <div class="form-group">
             <label for="your_sex">Sex</label>
-            <select class="form-control" id="your_sex"  name="entry.530954629">
-                <option>Select</option>
+            <select class="form-control" id="your_sex"  name="entry.1406660062" required>
+                <option value="">Select</option>
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
             </select>
         </div>
 
-        <!-- <div class="form-group">
-            <label for="your_sex">Sex</label>
-            <select class="form-control" id="your_sex" name="entry.138515813">
-                <option>Select</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-            </select>
-        </div> -->
-        <!-- <fieldset class="form-group">
-            <div class="row">
-            <legend class="col-form-label col-sm-12 pt-0">Sex</legend>
-            <div class="col-sm-12">
-                <div class="form-check">
-                <input class="form-check-input" type="radio" name="entry.762205503" id="gridRadiossex1" value="Male">
-                <label class="form-check-label" for="gridRadiossex1">
-                    Male
-                </label>
-                </div>
-                <div class="form-check">
-                <input class="form-check-input" type="radio" name="entry.762205503" id="gridRadiossex2" value="Female">
-                <label class="form-check-label" for="gridRadiossex2">
-                    Female
-                </label>
-                </div>
-                
-            </div>
-            </div>
-        </fieldset> -->
         <div class="form-group">
             <label for="your_phonenumber">Phone Number</label>
-            <input name="entry.1166974658" type="tel" class="form-control" id="your_phonenumber" placeholder="Enter phone number">
+            <input name="entry.822347425" type="tel" class="form-control" id="your_phonenumber" placeholder="Enter phone number" required>
         </div>
         <div class="form-group">
             <label for="your_city">City</label>
-            <input name="entry.657065569" type="text" class="form-control" id="your_city" placeholder="Enter city">
+            <input name="entry.231697965" type="text" class="form-control" id="your_city" placeholder="Enter city" required>
         </div>
       
         <fieldset class="form-group">
@@ -81,25 +53,25 @@ $('#exampleModal').modal('toggle')
             <legend class="col-form-label col-sm-12 pt-0">RadiHow did you come to know about this batch?os</legend>
             <div class="col-sm-12">
                 <div class="form-check">
-                <input class="form-check-input" type="radio" name="entry.1554937966" id="gridRadios1" value="Social Media (Facebook, Whatsapp, Instagram, Twitter)">
+                <input class="form-check-input" type="radio" name="entry.1051315673" id="gridRadios1" value="Social Media (Facebook, Whatsapp, Instagram, Twitter)" required>
                 <label class="form-check-label" for="gridRadios1">
                     Social Media (Facebook, Whatsapp, Instagram, Twitter)
                 </label>
                 </div>
                 <div class="form-check">
-                <input class="form-check-input" type="radio" name="entry.1554937966" id="gridRadios2" value="Friend">
+                <input class="form-check-input" type="radio" name="entry.1051315673" id="gridRadios2" value="Friend">
                 <label class="form-check-label" for="gridRadios2">
                     Friend
                 </label>
                 </div>
                 <div class="form-check">
-                <input class="form-check-input" type="radio" name="entry.1554937966" id="gridRadios2" value="Reference">
+                <input class="form-check-input" type="radio" name="entry.1051315673" id="gridRadios2" value="Reference">
                 <label class="form-check-label" for="gridRadios2">
                     Reference
                 </label>
                 </div>
                 <div class="form-check">
-                <input class="form-check-input" type="radio" name="entry.1554937966" id="gridRadios3" value="Other" >
+                <input class="form-check-input" type="radio" name="entry.1051315673" id="gridRadios3" value="Other" >
                 <label class="form-check-label" for="gridRadios3">
                     Other
                 </label>
@@ -109,7 +81,7 @@ $('#exampleModal').modal('toggle')
         </fieldset>
         <div class="form-group">
             <label for="your_expectations">What are your expectations?</label>
-            <input name="entry.367285302" type="text" class="form-control" id="your_expectations" placeholder="Enter expectations">
+            <input name="entry.583034435" type="text" class="form-control" id="your_expectations" placeholder="Enter expectations">
         </div>
         <button type="submit" name="submit" class="btn btn-orange">SUBMIT</button>
     </form>
